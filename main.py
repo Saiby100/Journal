d4ba678407862ebcb97c57c128ab1e623e8677e7
Saiby100 <Salahuddinsaiet.ss@gmail.com>
2022-01-27 14:06:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+from kivy.app import App
+from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from functools import partial
 
+class NotesApp(App):
+    def build(self):
+        global screen_manager
+        screen_manager = ScreenManager(transition=FadeTransition())
+        screen_manager.add_widget(Home())
+
+
+
+        return screen_manager
+
+class Home(Screen):
+    def build(self):
+        pass
+
+
+
+
+if __name__ == '__main__':
+
+    NotesApp().run()
